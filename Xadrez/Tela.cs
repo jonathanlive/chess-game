84dc34faa9b8ad0cad187c49ca36b0672cadbf56
Jonathan Silva <jonathanlive@outlook.com>
2018-07-31 16:45:11 -0300

    -Melhorando o metodo imprimir tabuleiro

--- Xadrez/Tela.cs
+++ Xadrez/Tela.cs
@@ -10,6 +10,7 @@ namespace Xadrez
         {
             for(int i =0; i<tab.linhas; i++)
             {
+                Console.Write(8 - i + " ");
                 for (int j = 0; j < tab.colunas; j++)
                 {
                     if (tab.peca(i, j) == null) {
@@ -17,11 +18,28 @@ namespace Xadrez
                     }
                     else
                     {
-                        Console.Write(tab.peca(i, j) + " ");
+                        Tela.imprimirPeca(tab.peca(i, j));
+                        Console.Write(" ");
                     }
                     
                 }
                 Console.WriteLine();
+            }
+            Console.WriteLine("  A B C D E F G H");
+        }
+
+        public static void imprimirPeca(Peca peca)
+        {
+            if (peca.cor == Cor.Branca)
+            {
+                Console.Write(peca);
+            }
+            else
+            {
+                ConsoleColor aux = Console.ForegroundColor;
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.Write(peca);
+                Console.ForegroundColor = aux;
             }
         }
     }
