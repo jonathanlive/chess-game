094a712ded8ae82db5f97b7fc38ed7b8cd8aea56
Jonathan Silva <jonathanlive@outlook.com>
2018-07-31 14:08:22 -0300

    Adicionado a classe posição na camada do tabuleiro

--- Xadrez/Program.cs
+++ Xadrez/Program.cs
@@ -1,15 +1,19 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
+using Tabuleiro;
 namespace Xadrez
 {
     class Program
     {
         static void Main(string[] args)
         {
+            Posicao p;
+
+            p = new Posicao(3,4);
+
+            Console.WriteLine("Posição: "+ p);
+
+            Console.ReadLine();
         }
     }
 }
