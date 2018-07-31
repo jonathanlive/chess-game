2ad6a930c19f650211dcb0424ac71f8c6189585c
Jonathan Silva <jonathanlive@outlook.com>
2018-07-31 14:37:54 -0300

    - Adicionado a classe tela, - Adicionado o método imprimir tabuleiro

--- Xadrez/Program.cs
+++ Xadrez/Program.cs
@@ -10,6 +10,8 @@ namespace Xadrez
 
             Tabuleiro tab = new Tabuleiro(8, 8);
 
+            Tela.imprimirTabuleiro(tab);
+
             Console.ReadLine();
         }
     }
--- Xadrez/Tabuleiro/Tabuleiro.cs
+++ Xadrez/Tabuleiro/Tabuleiro.cs
@@ -21,5 +21,9 @@ namespace tabuleiro
             pecas = new Peca[linhas, colunas];
         }
 
+        public Peca peca(int linha, int coluna)
+        {
+            return pecas[linha, coluna];
+        }
     }
 }
