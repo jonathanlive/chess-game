a33a8172609d2c63c5d1c3ad7eaf04630d9ad742
Jonathan Silva <jonathanlive@outlook.com>
2018-07-31 15:46:12 -0300

    -Adicionado a camada Xadrez, -Adicionado as classes Rei e Torre, -Adicionado o metodo colocar peca na classe tabuleiro

--- Xadrez/Program.cs
+++ Xadrez/Program.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-
+using xadrez;
 using tabuleiro;
 namespace Xadrez
 {
@@ -9,6 +9,10 @@ namespace Xadrez
         {
 
             Tabuleiro tab = new Tabuleiro(8, 8);
+
+            tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(0,0));
+            tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 3));
+            tab.colocarPeca(new Rei(tab, Cor.Preta), new Posicao(2, 4));
 
             Tela.imprimirTabuleiro(tab);
 
