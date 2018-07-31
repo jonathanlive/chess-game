6d4b951f3aac0792b0dae8f5ee9c585dc0a8b706
Jonathan Silva <jonathanlive@outlook.com>
2018-07-31 16:28:30 -0300

    -Adicionado a classe posição xadrez

---
+++ Xadrez/Xadrez/posicaoXadrez.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using tabuleiro;
+
+
+namespace xadrez
+{
+    class posicaoXadrez
+    {
+
+        public char coluna { get; set; }
+        public int linha { get; set; }
+
+        public posicaoXadrez(char coluna, int linha)
+        {
+            this.coluna = coluna;
+            this.linha = linha;
+        }
+
+        public Posicao toPosicao()
+        {
+            return new Posicao(8 - linha, coluna - 'a');
+        }
+
+
+        public override string ToString()
+        {
+            return "" + coluna + linha;
+        }
+    }
+}
