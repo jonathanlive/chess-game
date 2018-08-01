7bce9b22f2d7ad72927be4699b8aa087446eb1cd
Jonathan Silva <jonathanlive@outlook.com>
2018-08-01 13:26:29 -0300

    -Testes iniciais com movimentos possiveis das pecas

--- Xadrez/Tabuleiro/Peca.cs
+++ Xadrez/Tabuleiro/Peca.cs
@@ -1,7 +1,7 @@
 ﻿
 namespace tabuleiro
 {
-    class Peca
+   abstract class Peca
     {
         public Posicao posicao { get; set; }
         public Cor cor { get; protected set; }
@@ -22,6 +22,8 @@ namespace tabuleiro
         {
             qtd_movimentos++;
         }
+
+        public abstract bool[,] movimentosPossiveis();                                
 
     }
 }
